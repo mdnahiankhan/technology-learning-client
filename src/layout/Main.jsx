0ae7836127505273f2cf6b097a56b417587a701e
Md. Nahian <102530128+mdnahiankhan@users.noreply.github.com>
2022-10-25 12:46:36 +0600

    first commit

---
+++ src/layout/Main.jsx
@@ -0,0 +1,23 @@
+import { Container } from 'postcss';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from '../header/Header';
+import Leftside from '../leftside/Leftside';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <div className='grid grid-cols-3'>
+                <div className='col-span-1'>
+                    <Leftside></Leftside>
+                </div>
+                <div className='col-span-2'>
+                    <Outlet></Outlet>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/leftside/Leftside.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Leftside = () => {
+    return (
+        <div>
+            <h1>This is leftside nav</h1>
+        </div>
+    );
+};
+
+export default Leftside;
